b4424c7f921518e151e0fe67a37a9ba22b75755d
odnokurov <neagears@gmail.com>
2026-03-16 12:23:29 +0700

    added classes in classes

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -2,7 +2,27 @@
 
 namespace Src;
 
+use Error;
+
 class Application
 {
+    private Settings $settings;
 
+    public function __construct(Settings $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function __get($key)
+    {
+        if ($key === 'settings') {
+            return $this->settings;
+        }
+        throw new Error('Accessing a non-existent property');
+    }
+
+    public function run(): void
+    {
+        echo 'Working';
+    }
 }
