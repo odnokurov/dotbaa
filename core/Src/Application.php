caa5fa559f2a96e9d659b1e2dd3a41ef68925dc8
odnokurov <neagears@gmail.com>
2026-03-18 12:54:19 +0700

    made access rights

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -6,21 +6,19 @@ use Error;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Src\Auth\Auth;
 
 class Application
 {
     private Settings $settings;
     private Route $route;
     private Capsule $dbManager;
-    private Auth $auth;
 
     public function __construct(Settings $settings)
     {
         //Привязываем класс со всеми настройками приложения
         $this->settings = $settings;
         //Привязываем класс маршрутизации с установкой префикса
-        $this->route = new Route($this->settings->getRootPath());
+        $this->route = Route::single()->setPrefix($this->settings->getRootPath());
         //Создаем класс менеджера для базы данных
         $this->dbManager = new Capsule();
         //Создаем класс для аутентификации на основе настроек приложения
@@ -31,7 +29,6 @@ class Application
         //Инициализация класса пользователя на основе настроек приложения
         $this->auth::init(new $this->settings->app['identity']);
     }
-
     public function __get($key)
     {
         switch ($key) {
@@ -39,10 +36,9 @@ class Application
                 return $this->settings;
             case 'route':
                 return $this->route;
-            case 'auth':
-                return $this->auth;
+            default:
+                throw new Error('Accessing a non-existent property');
         }
-        throw new Error('Accessing a non-existent property');
     }
 
     private function dbRun()
@@ -55,7 +51,8 @@ class Application
 
     public function run(): void
     {
-        //Запуск маршрутизации
+        $this->dbRun();
+        $this->route->setPrefix($this->settings->getRootPath());
         $this->route->start();
     }
 }
